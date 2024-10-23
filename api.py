8409c2a25a49e40a7a6ca1529f0623f7c03fbfc5
rika5 <rika5637@mail.ru>
2024-10-24 02:53:28 +0300

    playing with vk api

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
 import requests
 
-response = requests.get('https://api.vk.com/method/users.get',
+response = requests.get('https://api.vk.com/method/users.get', #information from profile
 params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
         'v': 5.199,
-        'user_ids': 'prshkov'})
+        'user_ids': 'prshkov',
+        'fields': ['activities', 'about', 'books', 'career', 'contacts', 'city', 'education', 'exports', #дополнительные поля профиля
+                   'followers_count', 'friend_status', 'has_photo', 'home_town', 'sex', 'schools', 'status',
+                   'interests', 'military', 'personal', 'quotes', 'relation', 'relatives', 'universities', 'tv']})
 
+
+response2 = requests.get('https://api.vk.com/method/users.getSubscriptions', #подписки пользователя
+params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
+        'v': 5.199,
+        'user_id': 235408106, #integer only
+        'extended': 1}) #0/1
 
 print(response.json())
+print(response2.json())
 

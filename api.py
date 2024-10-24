cbe57468ea561472b67587baad48b79cc80a8831
rikkiter <rika5637@mail.ru>
2024-10-24 16:29:12 +0300

    playing with api

--- api.py
+++ api.py
@@ -15,6 +15,26 @@ params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442
         'user_id': 235408106, #integer only
         'extended': 1}) #0/1
 
+response3 = requests.get('https://api.vk.com/method/wall.get',
+params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
+        'owner_id': 450095816,
+        'v': 5.199,})
+
+response4 = requests.get('https://api.vk.com/method/wall.getComments',
+params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
+        'owner_id': 450095816,
+        'post_id': 1,
+        'v': 5.199,})
+
+response5 = requests.get('https://api.vk.com/method/utils.resolveScreenName',
+params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
+        'screen_name': 'prshkov',
+        'v': 5.199,})
+
 print(response.json())
 print(response2.json())
+print(response3.json())
+print(response4.json())
+print(response5.json())
+
 

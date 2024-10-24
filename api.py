2cbd4b1bf8a80392448c1bbf17ccf1962a785f5f
rikkiter <rika5637@mail.ru>
2024-10-24 17:57:06 +0300

    playing with api

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 response = requests.get('https://api.vk.com/method/users.get', #information from profile
 params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
         'v': 5.199,
-        'user_ids': 'prshkov',
+        'user_ids': 23880088,
         'fields': ['activities', 'about', 'books', 'career', 'contacts', 'city', 'education', 'exports', #дополнительные поля профиля
                    'followers_count', 'friend_status', 'has_photo', 'home_town', 'sex', 'schools', 'status',
                    'interests', 'military', 'personal', 'quotes', 'relation', 'relatives', 'universities', 'tv']})
@@ -22,13 +22,14 @@ params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442
 
 response4 = requests.get('https://api.vk.com/method/wall.getComments',
 params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
-        'owner_id': 450095816,
+        'owner_id': 273342306,
         'post_id': 1,
         'v': 5.199,})
 
-response5 = requests.get('https://api.vk.com/method/utils.resolveScreenName',
+response5 = requests.get('https://api.vk.com/method/photos.get',
 params={'access_token': 'fb82a857fb82a857fb82a85769f8a31d73ffb82fb82a8579c9bd442678f22dedb6df029',
-        'screen_name': 'prshkov',
+        'owner_id': 273342306,
+        'album_id': 'profile',
         'v': 5.199,})
 
 print(response.json())
